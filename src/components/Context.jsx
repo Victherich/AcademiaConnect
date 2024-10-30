e68521f75e59cd726f25ca2c1b9e7f680afd7580
Victherich <victherich@gmail.com>
2024-10-30 05:36:40 +0100

    a

--- src/components/Context.jsx
+++ src/components/Context.jsx
@@ -8,6 +8,7 @@ const ContextProvider = ({children}) => {
 const [Admin,setAdmin]=useState(false)
 const [student,setStudent]=useState(false)
 const [teacher,setTeacher]=useState(false)
+const check = ''
 
   return (
     <Context.Provider value={{Admin,setAdmin,student,setStudent,teacher,setTeacher}}>
